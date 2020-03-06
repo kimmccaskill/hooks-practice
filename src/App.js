61f04ad0f44f72e98bf060cea825b74481950082
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-06 14:03:56 -0700

    Add saveIdea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import Form from './Form'
+import IdeasContainer from './IdeaContainer'
 import './App.css';
 
 function App() {
+  const [ideas, setIdeas] = useState({ideas: []});
+
+  const saveIdea = (idea) => {
+    setIdeas({ideas: [...ideas.ideas, idea]})
+  }
+
   return (
     <main className="App">
       <h1>Ideabox using React Hooks</h1>
-      <Form />
+      <Form saveIdea={saveIdea}/>
+      <IdeasContainer ideas={ideas.ideas} />
     </main>
   );
 }
