4af878426dea273ab7d2dc36f2bcbad66c51df7f
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-06 14:10:46 -0700

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import './App.css';
 function App() {
   const [ideas, setIdeas] = useState({ideas: []});
 
-  const saveIdea = (idea) => {
+  const saveIdea = idea => {
     setIdeas({ideas: [...ideas.ideas, idea]})
+  }
+
+  const deleteIdea = id => {
+    setIdeas({ideas: ideas.ideas.filter(idea => idea.id !== id)})
   }
 
   return (
     <main className="App">
       <h1>Ideabox using React Hooks</h1>
       <Form saveIdea={saveIdea}/>
-      <IdeasContainer ideas={ideas.ideas} />
+      <IdeasContainer ideas={ideas.ideas} deleteIdea={deleteIdea}/>
     </main>
   );
 }
